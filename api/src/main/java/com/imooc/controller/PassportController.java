069e1a02a60907b405d1b6a7a26fa2631e79e04d
张喜硕 <zhangxishuo1998@outlook.com>
2020-04-30 20:16:47 +0800

    swagger2

--- api/src/main/java/com/imooc/controller/PassportController.java
+++ api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,12 +1,10 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
 import com.imooc.utils.JsonResult;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("passport")
@@ -27,6 +25,36 @@ public class PassportController {
         if (exist) {
             return JsonResult.error("用户名已经存在");
         }
+        return JsonResult.ok();
+    }
+
+    @PostMapping("register")
+    public JsonResult register(@RequestBody UserBO userBO) {
+        String username = userBO.getUsername();
+        String password = userBO.getPassword();
+        String confirmPwd = userBO.getConfirmPassword();
+
+        if (StringUtils.isBlank(username) ||
+                StringUtils.isBlank(password) ||
+                StringUtils.isBlank(confirmPwd)) {
+            return JsonResult.error("用户名密码不能为空");
+        }
+
+        boolean exist = userService.usernameExists(username);
+        if (exist) {
+            return JsonResult.error("用户名已经存在");
+        }
+
+        if (password.length() < 6) {
+            return JsonResult.error("密码长度不能小于6位");
+        }
+
+        if (!password.equals(confirmPwd)) {
+            return JsonResult.error("两次密码输入不一致");
+        }
+
+        userService.createUser(userBO);
+
         return JsonResult.ok();
     }
 }
