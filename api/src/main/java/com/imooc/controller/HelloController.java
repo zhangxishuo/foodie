29fbd2ca3cf1bcf5dc8b54db7b159ea6b98a2f5c
张喜硕 <zhangxishuo1998@outlook.com>
2020-05-02 09:40:07 +0800

    session

--- api/src/main/java/com/imooc/controller/HelloController.java
+++ api/src/main/java/com/imooc/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @ApiIgnore
 @RestController
 public class HelloController {
@@ -11,5 +14,15 @@ public class HelloController {
     @GetMapping("/hello")
     public Object hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "user-admin");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+        session.removeAttribute("userInfo");
+        return "OK";
     }
 }
