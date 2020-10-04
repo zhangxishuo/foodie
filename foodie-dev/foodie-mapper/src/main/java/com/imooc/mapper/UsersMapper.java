ab34dd09e6d65d6922566a68df4808fd828af3de
zhangxishuo <zhangxishuo1998@outlook.com>
2020-10-04 09:25:35 +0800

    1.3.3

--- foodie-dev/foodie-mapper/src/main/java/com/imooc/mapper/UsersMapper.java
+++ foodie-dev/foodie-mapper/src/main/java/com/imooc/mapper/UsersMapper.java
@@ -2,6 +2,8 @@ package com.imooc.mapper;
 
 import com.imooc.mapper.core.CoreMapper;
 import com.imooc.pojo.Users;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UsersMapper extends CoreMapper<Users> {
 }
