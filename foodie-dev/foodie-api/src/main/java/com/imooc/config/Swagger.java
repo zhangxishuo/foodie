e96f3cb40879dc1de39c6dd33f1e384ca54653a1
zhangxishuo <zhangxishuo1998@outlook.com>
2020-10-04 16:02:45 +0800

    swagger

---
+++ foodie-dev/foodie-api/src/main/java/com/imooc/config/Swagger.java
@@ -0,0 +1,41 @@
+package com.imooc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+public class Swagger {
+
+    /**
+     * /swagger-ui/index.html
+     * /doc.html
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.imooc.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("天天吃货 电商平台接口 API")
+                .contact(new Contact("imooc", "https://www.imooc.com", "api@imooc.com"))
+                .description("天天吃货 API 文档")
+                .version("1.0.0")
+                .termsOfServiceUrl("https://www.imooc.com")
+                .build();
+    }
+}
