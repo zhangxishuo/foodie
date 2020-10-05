90ee40df619cc001f221cd7602e10db15972552c
zhangxishuo <zhangxishuo1998@outlook.com>
2020-10-05 16:15:15 +0800

    1.3.12

--- foodie-dev/foodie-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-dev/foodie-api/src/main/java/com/imooc/controller/HelloController.java
@@ -3,11 +3,22 @@ package com.imooc.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class HelloController {
 
     @GetMapping("hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("setSession")
+    public String setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "New User");
+        session.setMaxInactiveInterval(3600);
+        return "OK";
     }
 }
