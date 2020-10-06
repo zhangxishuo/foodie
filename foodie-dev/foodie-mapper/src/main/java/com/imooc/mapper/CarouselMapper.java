31efd3461a2dee6276b1ca4dfc7c019561218b7e
zhangxishuo <zhangxishuo1998@outlook.com>
2020-10-06 16:13:41 +0800

    2.1.1

--- foodie-dev/foodie-mapper/src/main/java/com/imooc/mapper/CarouselMapper.java
+++ foodie-dev/foodie-mapper/src/main/java/com/imooc/mapper/CarouselMapper.java
@@ -2,6 +2,8 @@ package com.imooc.mapper;
 
 import com.imooc.mapper.core.CoreMapper;
 import com.imooc.pojo.Carousel;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface CarouselMapper extends CoreMapper<Carousel> {
 }
---
+++ foodie-dev/foodie-service/src/main/java/com/imooc/service/CarouselService.java
@@ -0,0 +1,10 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Carousel;
+
+import java.util.List;
+
+public interface CarouselService {
+
+    List<Carousel> queryAll(Integer isShow);
+}
