6a7fe05bff66b6b41e0cc8861a731641e60c5b8a
zhangxishuo <zhangxishuo1998@outlook.com>
2020-11-01 09:40:22 +0800

    3.3.4

--- foodie-dev/foodie-api/src/main/java/com/imooc/controller/OrdersController.java
+++ foodie-dev/foodie-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -3,8 +3,10 @@ package com.imooc.controller;
 import com.imooc.bo.SubmitOrderBO;
 import com.imooc.common.IMOOCJSONResult;
 import com.imooc.enums.PayMethod;
+import com.imooc.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,9 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class OrdersController {
 
+    @Autowired
+    private OrderService orderService;
+
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
     @PostMapping("create")
     public IMOOCJSONResult create(@RequestBody SubmitOrderBO submitOrderBO,
@@ -27,6 +32,9 @@ public class OrdersController {
                 && !submitOrderBO.getPayMethod().equals(PayMethod.ALIPAY.type)) {
             return IMOOCJSONResult.errorMsg("支付方式不支持！");
         }
+
+        // 1. 创建订单
+        orderService.createOrder(submitOrderBO);
 
         return IMOOCJSONResult.ok();
     }
